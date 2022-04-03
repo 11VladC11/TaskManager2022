94044733d7a396bdb319ddfa1ac0dfe0da27623d
11VladC11 <vladw239@gmail.com>
2022-04-03 17:32:56 +0300

    added pop up task

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,3 +3,76 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
+const tasksDOM = document.querySelector('ul.all-tasks');
+const loadingDOM = document.querySelector('.loading-text');
+const formDOM = document.querySelector('.task-form');
+const taskInputDOM = document.querySelector('.task-input');
+const formAlertDOM = document.querySelector('.form-alert');
+const taskCompletedDOM = document.querySelector('#checkbox')
+const showTask = async ()=>{
+	loadingDOM.style.visibility = 'visible';
+	try{
+		const {data:{tasks},} = await axios.get('/api/v1/tasks')
+		const allTasks = tasks.map((task)=>{
+			const {completed, _id: taskID, name} = task;
+			return `<li>
+			<div class="task-info">
+				<div class="task-status">
+					<input id="checkbox" type="checkbox">
+					<label for="checkbox">
+						<div class="radio-check checked"></div>
+					</label>
+					<img class="checked" src="@img/icon-check.svg" alt="">
+				</div>
+				<div class="task-title ${completed}}">
+					${name}
+				</div>
+			</div>
+			<button type="button" class="delete-btn" data-id="${taskID}">
+				<img src="@img/icon-cross.svg" alt="">
+			</button>
+		</li>`
+		}).join('')
+		tasksDOM.innerHTML = allTasks
+	}catch(error){
+		// tasksDOM.innerHTML= '<h5 class="empty-list">There was an error, please try later....</h5>'
+	}
+	loadingDOM.style.visibility='hidden';
+}
+showTask();
+
+tasksDOM.addEventListener('click', async(e)=>{
+	const el = e.target;
+	if(el.parentElement.classList.contains('delete-btn')){
+		loadingDOM.style.visibility = 'visible';
+		const id = el.parentElement.dataset.id;
+		try{
+			await axios.delete(`/api/v1/tasks/${id}`)
+			showTask()
+		}catch(error){
+			console.log(error);
+		}
+	}
+	loadingDOM.style.visibility ='hidden';
+})
+
+formDOM.addEventListener('click', async(e)=>{
+	e.preventDefault()
+	const name =taskInputDOM.value;
+	try{
+		await axios.post(`/api/v1/tasks, ${name}`)
+		showTask()
+		taskInputDOM.value='';
+		formAlertDOM.style.display = 'block'
+		formAlertDOM.textContent=`succes, task added`
+		formAlertDOM.classList.add('text-succes')
+	}catch(error){
+		formAlertDOM.style.display ="block";
+		formAlertDOM.innerHTML = 'error, please try again'
+	}
+	setTimeout(()=>{
+		formAlertDOM.style.display = 'none'
+		formAlertDOM.classList.remove('text-success')
+	},3000)
+})
+
