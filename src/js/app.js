9772db1e90ae360448d5088dc1236e3e4e757252
11VladC11 <vladw239@gmail.com>
2022-04-10 08:09:20 +0300

    added edit task separate

--- src/js/app.js
+++ src/js/app.js
@@ -191,5 +191,178 @@ import "./libs/dynamic_adapt.js";
 // Прочее ========================================================================================================================================================================================================================================================
 // ========================================================================================================================================================================================================================================================
 /* Подключаем файлы со своим кодом */
+
+const taskstatus = document.querySelector('.task-edit-completed');
+taskstatus.addEventListener('click',()=>{
+	if(!taskstatus.checked == true){
+		document.querySelector('.content-task-edit .radio-check').classList.remove('checked');
+		document.querySelector('.content-task-edit img').classList.remove('checked')
+
+	}else{
+		document.querySelector('.content-task-edit .radio-check').classList.add('checked');
+		document.querySelector('.content-task-edit img').classList.add('checked')
+	}
+})
+
+const tasksDOM = document.querySelector('div.all-tasks');
+const loadingDOM = document.querySelector('.loading-text');
+const formDOM = document.querySelector('.task-form');
+const taskInputDOM = document.querySelector('.task-input');
+const formAlertDOM = document.querySelector('.form-alert');
+// Load tasks from /api/tasks
+const showTasks = async () => {
+  loadingDOM.style.visibility = 'visible'
+  try {
+    const {data: { tasks },} = await axios.get('/api/v1/tasks')
+    if (tasks.length < 1) {
+      tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>'
+      loadingDOM.style.visibility = 'hidden'
+      return
+    }
+    const allTasks = tasks.map((task) => {
+        const { completed, _id: taskID, name } = task
+        return `
+		  <li>
+				<div class="task-info">
+					<div class="task-status">
+						<input id="checkbox" type="checkbox">
+					<label for="checkbox">
+						<div class="radio-check checked"></div>
+					</label>
+					<img class="checked" src="@img/icon-check.svg" alt="">
+				</div>
+				<div class="task-title ${completed && 'checked'}">
+					${name}
+				</div>
+			</div>
+			<div class="singular-control">
+				<div class="edit-task">
+					<a href="./edit.html?id=${taskID}">
+						<button class="task-edit">Edit</button>
+					</a>
+			</div>
+			<div class="delete-task">
+				<button type="button" class="delete-btn" data-id="${taskID}" >
+					<img src="@img/icon-cross.svg" alt="">
+				</button>
+			</div>
+		</div>
+	</li>
+`
+      })
+      .join('')
+    tasksDOM.innerHTML = allTasks
+  } catch (error) {
+    tasksDOM.innerHTML =
+      '<h5 class="empty-list">There was an error, please try later....</h5>'
+  }
+  loadingDOM.style.visibility = 'hidden'
+}
+
+showTasks()
+
+// delete task /api/tasks/:id
+
+tasksDOM.addEventListener('click', async (e) => {
+  const el = e.target
+  if (el.parentElement.classList.contains('delete-btn')) {
+    loadingDOM.style.visibility = 'visible'
+    const id = el.parentElement.dataset.id
+    try {
+      await axios.delete(`/api/v1/tasks/${id}`)
+      showTasks()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  loadingDOM.style.visibility = 'hidden'
+})
+
+// form
+
+formDOM.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const name = taskInputDOM.value
+
+  try {
+    await axios.post('/api/v1/tasks', { name })
+    showTasks()
+    taskInputDOM.value = ''
+    formAlertDOM.style.display = 'block'
+    formAlertDOM.textContent = `success, task added`
+    formAlertDOM.classList.add('text-success')
+  } catch (error) {
+    formAlertDOM.style.display = 'block'
+    formAlertDOM.innerHTML = `error, please try again`
+  }
+  setTimeout(() => {
+    formAlertDOM.style.display = 'none'
+    formAlertDOM.classList.remove('text-success')
+  }, 3000)
+})
+
+
+
+
+//edit task
+
+const taskIDDOM = document.querySelector('.task-edit-id');
+const taskNameDOM = document.querySelector('task-edit-name');
+const taskCompletedDOM =  document.querySelector('.task-edit-completed');
+const editFormDOM = document.querySelector('.single-task-form');
+const editBtnDOM = document.querySelector('.task-edit-btn');
+const editAlertDOM = document.querySelector('.edit-alert');
+const params = window.location.search;
+const id = new URLSearchParams(params).get('id');
+let tempName;
+
+const showEditTask = async () =>{
+	try{
+		const {data:{task},} =await axios.get(`/api/v1/tasks/${id}`)
+		const {_id:taskID,completed,name} = task;
+		taskIDDOM.textContent=taskID
+		taskNameDOM.value = name
+		tempName = name
+		if(completed){
+			taskCompletedDOM.checked = true;
+		} 
+	
+	} catch(error){
+		console.log(error)
+	}
+}
+showEditTask();
+editFormDOM.addEventListener('submit',async (e)=>{
+	editBtnDOM.textContent = 'Loading...'
+	e.preventDefault();
+	try{
+		const taskName = taskNameDOM.value;
+		const taskCompleted = taskCompletedDOM.checked
+		const {data:{task},} = await axios.patch('/api/v1/tasks/${id}',{
+			name:taskName,
+			completed: taskCompleted,
+		})
+		const {_id:taskID,completed,name} = task;
+		taskIDDOM.textContent = taskID
+		taskNameDOM.value = name
+		tempName = name
+		if(completed){
+			taskCompletedDOM.checked = true;
+		}
+		editAlertDOM.style.display ='block';
+		editAlertDOM.textContent= 'succes, edited task';
+		editAlertDOM.classList.add('text-success')
+	} catch (error){
+		console.error(error)
+		taskNameDOM.value = tempName
+		editAlertDOM.style.display ='block'
+		editAlertDOM.innerHTML = 'error, please try again' 
+	}
+	editBtnDOM.textContent = 'Edit'
+	setTimeout(()=>{
+		editAlertDOM.style.display = 'none'
+		editAlertDOM.classList.remove('text-success')
+	},3000)
+})
 import "./files/script.js";
 //============================================================================================================================================================================================================================================
